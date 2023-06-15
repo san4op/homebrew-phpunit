9419fb39342ef507ea5649d30e2e868d220b0ba9
Alexander Pushkin <a@san4op.pro>
2023-06-15 22:01:20 +0300

    add phpunit 7.5.20

---
+++ Formula/phpunit@7.5.rb
@@ -0,0 +1,121 @@
+# Original formulae by homebrew-core
+
+class PhpunitAT75 < Formula
+  desc "Programmer-oriented testing framework for PHP"
+  homepage "https://phpunit.de"
+  url "https://phar.phpunit.de/phpunit-7.5.20.phar"
+  sha256 "472b6e275e4479bab71f2f7744d2e22ace9566a4e3cef37952e4d9c07f6d3a92"
+  license "BSD-3-Clause"
+
+  keg_only :versioned_formula
+
+  deprecate! date: "2020-02-07", because: :deprecated_upstream
+
+  depends_on "shivammathur/php/php@7.3"
+
+  patch do
+    url "https://raw.githubusercontent.com/san4op/homebrew-formula-patches/master/phpunit/7.5.20.patch"
+    sha256 "e58352bf7a30206f49b2bbc268fcfe9d46857e140abd5fb7383522e1bd0dc888"
+  end
+
+  def install
+    bin.install "phpunit-#{version}.phar" => "phpunit"
+  end
+
+  test do
+    (testpath/"src/autoload.php").write <<~EOS
+      <?php
+      spl_autoload_register(
+          function($class) {
+              static $classes = null;
+              if ($classes === null) {
+                  $classes = array(
+                      'email' => '/Email.php'
+                  );
+              }
+              $cn = strtolower($class);
+              if (isset($classes[$cn])) {
+                  require __DIR__ . $classes[$cn];
+              }
+          },
+          true,
+          false
+      );
+    EOS
+
+    (testpath/"src/Email.php").write <<~EOS
+      <?php
+        declare(strict_types=1);
+
+        final class Email
+        {
+            private $email;
+
+            private function __construct(string $email)
+            {
+                $this->ensureIsValidEmail($email);
+
+                $this->email = $email;
+            }
+
+            public static function fromString(string $email): self
+            {
+                return new self($email);
+            }
+
+            public function __toString(): string
+            {
+                return $this->email;
+            }
+
+            private function ensureIsValidEmail(string $email): void
+            {
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    throw new InvalidArgumentException(
+                        sprintf(
+                            '"%s" is not a valid email address',
+                            $email
+                        )
+                    );
+                }
+            }
+        }
+    EOS
+
+    (testpath/"tests/EmailTest.php").write <<~EOS
+      <?php
+      declare(strict_types=1);
+
+      use PHPUnit\\Framework\\TestCase;
+
+      final class EmailTest extends TestCase
+      {
+          public function testCanBeCreatedFromValidEmailAddress(): void
+          {
+              $this->assertInstanceOf(
+                  Email::class,
+                  Email::fromString('user@example.com')
+              );
+          }
+
+          public function testCannotBeCreatedFromInvalidEmailAddress(): void
+          {
+              $this->expectException(InvalidArgumentException::class);
+
+              Email::fromString('invalid');
+          }
+
+          public function testCanBeUsedAsString(): void
+          {
+              $this->assertEquals(
+                  'user@example.com',
+                  Email::fromString('user@example.com')
+              );
+          }
+      }
+
+    EOS
+    assert_match(/^OK \(3 tests, 3 assertions\)$/,
+      shell_output("#{bin}/phpunit --bootstrap src/autoload.php tests/EmailTest"))
+  end
+end
